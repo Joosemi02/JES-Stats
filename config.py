285e6344bbe8c9fae21ad8bdf300f87c76ab91eb
Joosemi02 <37875402+Joosemi02@users.noreply.github.com>
2022-01-13 22:31:50 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,18 @@
+import nextcord as discord
+from motor import motor_tornado
+
+token = os.environ["token"]
+
+client = motor_tornado.MotorClient(os.environ["db_token"]))
+db = client["jesbot"]
+
+EMBED_COLOR = discord.Color.blue()
+
+api_1 = os.environ["api_1"]
+api_2 = os.environ["api_2"]
+api_3 = os.environ["api_3"]
+api_4 = os.environ["api_4"]
+api_5 = os.environ["api_5"]
+api_6 = os.environ["api_6"]
+api_7 = os.environ["api_7"]
+api_8 = os.environ["api_8"]
