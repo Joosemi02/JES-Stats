7688b1c89ca1f7cefeeedd63f9b1ab9085f950cf
Joosemi02 <josemiguel.dark@gmail.com>
2022-01-13 22:26:28 +0100

    Created base bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,10 @@
 import nextcord as discord, requests, os
-from nextcord.ext import commands, tasks
+from nextcord.ext import commands
 from typing import Union
+from config import token, db, EMBED_COLOR
 
-EMBED_COLOR = discord.Color.blue()
+linked = db["linked"]
+
 
 async def get_embed(ctx: commands.Context, title=None, description=None, color=None):
     if not color:
@@ -16,12 +18,14 @@ async def get_embed(ctx: commands.Context, title=None, description=None, color=N
     embed.set_author(
         name=ctx.message.author.display_name, icon_url=ctx.author.avatar.url
     )
-    embed.set_thumbnail(url="https://cdn.discordapp.com/icons/911944157625483264/359028bbc374ee43f60e25e35c39874b.png?size=1024")
+    embed.set_thumbnail(
+        url="https://cdn.discordapp.com/icons/911944157625483264/359028bbc374ee43f60e25e35c39874b.png?size=1024"
+    )
     return embed
 
 
 async def is_server_online(ctx):
-    res = requests.get(os.api_4)
+    res = requests.get(os.environ["api_4"])
     active = res.json()["serverOnline"]
     if active:
         return True
@@ -30,8 +34,6 @@ async def is_server_online(ctx):
             ctx, ":red_circle: The server is offline, can't find that info"
         )
     )
-  else:
-    return False
 
 
 async def find_linked(user: Union[discord.User, int, commands.Context]):
@@ -41,7 +43,7 @@ async def find_linked(user: Union[discord.User, int, commands.Context]):
         id_ = user.id
     else:
         id_ = user
-    players = await links.find_one({"_id": "players"})
+    players = await linked.find_one({"_id": "players"})
     if id_ in players:
         return players["ign"]
     else:
@@ -53,18 +55,23 @@ intents = discord.Intents.all()
 bot = commands.Bot(
     command_prefix="/",
     intents=intents,
-    help_command=CustomHelpCommand(),
+    help_command=None,
     case_insensitive=True,
 )
-DiscordComponents(bot)
+
 
 @bot.event
 async def on_ready():
     print(f"{bot.user.name}: Bot loaded successfully. ID: {bot.user.id}")
-    await bot.change_presence(status=nextcord.Status.online, activity=nextcord.Activity(type=nextcord.ActivityType.watching, name="how I'm being developed.")
+    await bot.change_presence(
+        status=discord.Status.online,
+        activity=discord.Activity(
+            type=discord.ActivityType.watching, name="how I'm being developed."
+        ),
+    )
 
 
 if __name__ == "__main__":
-    #bot.load_extension("cogs.commands")
+    # bot.load_extension("cogs.commands")
 
     bot.run(token)
