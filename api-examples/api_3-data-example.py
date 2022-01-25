5718448dfd8dc2080f8f8453d240228cec619616
Joosemi02 <josemiguel.dark@gmail.com>
2022-01-25 12:15:19 +0100

    Created api 3 example, print api 4

---
+++ api-examples/api_3-data-example.py
@@ -0,0 +1,9 @@
+li = [
+    {
+        "name": "EtrnlEngineer",
+        "town": "cherub",
+        "nation": "lovozaro",
+        "rank": "Resident",
+    },
+    {"name": "Mr_Xbomx", "town": "cherub", "nation": "lovozaro", "rank": "Resident"},
+]
