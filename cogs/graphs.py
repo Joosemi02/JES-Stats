b8ecc1b0a28136af08c66388c8e8130533735d99
Joosemi02 <josemiguel.dark@gmail.com>
2022-01-25 19:32:45 +0100

    Testing graph creation with matplotlib

--- cogs/graphs.py
+++ cogs/graphs.py
@@ -1,8 +1,12 @@
 import asyncio
-import matplotlib
+import matplotlib as mpl
+import matplotlib.image as mpi
+import matplotlib.pyplot as plt
 import nextcord as discord
+import numpy as np
 import requests
-from datetime import datetime, timezone
+import schedule
+from datetime import datetime
 from json import JSONDecodeError
 from nextcord.ext import commands, tasks
 from config import db, api_5
@@ -32,6 +36,8 @@ class Graphs(commands.Cog):
             spain = 0
             online = 0
         n = datetime.now()
+        if n.minute == 0:
+            n.minute = 00
         time = f"{n.hour+1}:{n.minute}"
         day = n.strftime("%Y/%m/%d")
         try:
@@ -49,8 +55,43 @@ class Graphs(commands.Cog):
     @get_data.before_loop
     async def before_task(self):
         await self.bot.wait_until_ready()
-        while datetime.now().minute not in (00, 15, 30, 45):
+        while datetime.now().minute not in (0, 15, 30, 45):
             await asyncio.sleep(60)
+
+    async def make_graph(self):
+        data = await graphs.find_one({"_id": datetime.now().strftime("%Y/%m/%d")})
+        spain = []
+        online = []
+        for n in data.keys():
+            spain.append(data[n]["spain"])
+            online.append(data[n]["online"])
+        fig = plt.figure()
+        fig, ax, bx = plt.subplots(2, 2)
+        ax.plot(data.keys(), spain)
+        bx.plot(data.keys(), online)
+        fig.show()
+        storage = self.bot.get_channel(935596324127129740)
+        await storage.send(file=fig)
+
+    async def get_graph(self):
+        storage = self.bot.get_channel(935596324127129740)
+        for m in storage.history(1).flatten():
+            m: discord.Message
+            for a in m.attachments:
+                return a.url
+
+    @commands.command(name="graph")
+    async def send_graph(self):
+        c: discord.TextChannel = self.bot.get_channel(
+            871434957308985374
+        )  # 851216632155865161
+        graph = await self.get_graph()
+        embed = discord.Embed()
+        embed.set_author(name=c.guild.name, icon_url=c.guild.icon.url)
+        embed.set_image(url="attachments://graph.png")
+        await c.send(embed=embed, file=graph)
+
+    schedule.every().day.at("00:00").do(send_graph)
 
 
 def setup(bot):
