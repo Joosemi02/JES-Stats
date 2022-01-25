2223c6c9d88efeae48a74c26dda734aebad7048c
Joosemi02 <josemiguel.dark@gmail.com>
2022-01-25 10:21:35 +0100

    Test to understand api requests better

--- cogs/graphs.py
+++ cogs/graphs.py
@@ -1,8 +1,9 @@
 import matplotlib
 import nextcord as discord
+import requests
 from asyncio import tasks
 from nextcord.ext import commands
-from config import db
+from config import db, api_1, api_2, api_3, api_4, api_5, api_6, api_7, api_8
 
 graphs = db["graphs"]
 
@@ -17,6 +18,17 @@ class Graphs(commands.Cog):
     @tasks.loop()
     async def get_data():
         pass
+    
+    @commands.command()
+    async def test(self, ctx):
+        print(requests.get(api_1))
+        print(requests.get(api_2))
+        print(requests.get(api_3))
+        print(requests.get(api_4))
+        print(requests.get(api_5))
+        print(requests.get(api_6))
+        print(requests.get(api_7))
+        print(requests.get(api_8))
 
 def setup(bot):
     bot.add_cog(Graphs(bot))
