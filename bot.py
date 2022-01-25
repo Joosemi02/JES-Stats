ab03eb7799716c25852e2519dbb24675fd3151c1
Joosemi02 <josemiguel.dark@gmail.com>
2022-01-25 11:02:39 +0100

    Printed first api to analize info

--- bot.py
+++ bot.py
@@ -56,7 +56,7 @@ async def guilds(ctx: commands.Context):
 
 @bot.command()
 async def testo(ctx):
-    await ctx.send(await bot.application_info().members)
+    await ctx.send(await bot.application_info())
 
 
 @bot.command()
--- cogs/graphs.py
+++ cogs/graphs.py
@@ -6,10 +6,11 @@ from config import db, api_1, api_2, api_3, api_4, api_5, api_6, api_7, api_8
 
 graphs = db["graphs"]
 
+
 class Graphs(commands.Cog):
     def __init__(self, bot):
         self.bot: commands.Bot = bot
-    
+
     @commands.Cog.listener()
     async def on_ready(self):
         print(f"{self.bot.user.name}: The graphs extension was loaded successfully.")
@@ -17,17 +18,18 @@ class Graphs(commands.Cog):
     @tasks.loop(minutes=10)
     async def get_data():
         nations = 0
-    
+
     @commands.command()
     async def test(self, ctx):
         print(requests.get(api_1).json())
-        print(requests.get(api_2).json())
-        print(requests.get(api_3).json())
-        print(requests.get(api_4).json())
-        print(requests.get(api_5).json())
-        print(requests.get(api_6).json())
-        print(requests.get(api_7).json())
-        print(requests.get(api_8).json())
+        # print(requests.get(api_2).json())
+        # print(requests.get(api_3).json())
+        # print(requests.get(api_4).json())
+        # print(requests.get(api_5).json())
+        # print(requests.get(api_6).json())
+        # print(requests.get(api_7).json())
+        # print(requests.get(api_8).json())
+
 
 def setup(bot):
     bot.add_cog(Graphs(bot))
