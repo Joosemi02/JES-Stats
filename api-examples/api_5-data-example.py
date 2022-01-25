6f27ac2fc33dde41c6169003268fa21dec91dc40
Joosemi02 <josemiguel.dark@gmail.com>
2022-01-25 12:41:26 +0100

    Build get_data task based on api 5. Created api 5 example. Printing api 6

---
+++ api-examples/api_5-data-example.py
@@ -0,0 +1,24 @@
+[
+    {
+        "x": 33237,
+        "y": 64,
+        "z": -11510,
+        "isUnderground": False,
+        "nickname": "Cool_Charlie",
+        "name": "Cool_Charlie",
+        "town": "Fire",
+        "nation": "Russia",
+        "rank": "Resident",
+    },
+    {
+        "x": 0,
+        "y": 64,
+        "z": 0,
+        "isUnderground": True,
+        "nickname": "_crysisss9868",
+        "name": "_crysisss9868",
+        "town": "Trkiye",
+        "nation": "No Nation",
+        "rank": "Resident",
+    }
+]
--- cogs/graphs.py
+++ cogs/graphs.py
@@ -1,8 +1,9 @@
+from datetime import date
 import matplotlib
 import nextcord as discord
 import requests
 from nextcord.ext import commands, tasks
-from config import db, api_1, api_2, api_3, api_4, api_5, api_6, api_7
+from config import db, api_5, api_6, api_7
 
 graphs = db["graphs"]
 
@@ -17,12 +18,29 @@ class Graphs(commands.Cog):
 
     @tasks.loop(minutes=10)
     async def get_data():
-        nations = 0
+        res = requests.get(api_5).json()
+        spain = sum(
+            ("nation" in res[i] and res[i]["nation"].lower() == "spanish_republic")
+            for i in range(len(res))
+        )
+        online = sum("nation" in res[i] for i in range(len(res)))
+        time = date.timetuple()
+        day = date.today()
+        try:
+            await graphs.insert_one(
+                {"_id": day, f"{time}": {"spain": spain, "online": online}}
+            )
+        except:
+            await graphs.update_one(
+                {
+                    "_id": day,
+                    "$set": {f"{time}": {"spain": spain, "online": online}},
+                }
+            )
 
     @commands.command()
     async def test(self, ctx):
-        print(requests.get(api_5).json())
-        # print(requests.get(api_6).json())
+        print(requests.get(api_6).json())
         # print(requests.get(api_7).json())
 
 
